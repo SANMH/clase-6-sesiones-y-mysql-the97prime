3754e2d244eb045f45791a2ec3dd20e97a9aa060
Oscar Mateo Peñaherrera <osmap_1997@gmail.com>
2019-05-13 15:17:31 -0500

    Ejercicio 5

--- ejercicio5/index.php
+++ ejercicio5/index.php
@@ -14,3 +14,30 @@
 * En la página "modificar.php" presentar el formulario que permita modificar la información del alumno seleccionado.
 *
 */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulario alumnos</title>
+</head>
+<body>
+<h1 align ="center">Registro de alumnos</h1>
+<form action="nuevo.php" method = "post">
+<div>
+    <label>Nombre: </label>
+    <input type="text" name ="nombreAlum" required="required">
+</div>
+<div>
+    <label>Mail: </label>
+    <input type="email" name="email" required="required">
+</div>
+<div>
+    <label>Código curso: </label>
+    <input type="text" name = "codCurso" required="required">
+</div>
+<button>Registrar alumno</button>
+</form>    
+</body>
+</html>
